065a2efa84d2ac14b4ade766ef5cfb5c964fdca9
Samuraipavlo <61064424+Samuraipavlo@users.noreply.github.com>
2020-03-23 19:19:39 +0200

    content and slides

---
+++ js/script.js
@@ -0,0 +1,29 @@
+ function myFunction() {
+	var slider = document.getElementById('contant')
+	contant.style.backgroundImage = "url('img/stine.jpg')";
+}
+
+ function myFunction_1() {
+	var slider = document.getElementById('contant')
+	contant.style.backgroundImage = "url('img/ocean.jpg')";
+}
+
+ function myFunction_2() {
+	var slider = document.getElementById('contant')
+	contant.style.backgroundImage = "url('img/neon.jfif')";
+}
+
+ function myFunction_3() {
+	var slider = document.getElementById('contant')
+	contant.style.backgroundImage = "url('img/lion.jpg')";
+}
+
+ function myFunction_4() {
+	var slider = document.getElementById('contant')
+	contant.style.backgroundImage = "url('img/img.jfif')";
+}
+
+ function myFunction_5() {
+	var slider = document.getElementById('contant')
+	contant.style.backgroundImage = "url('img/images.jfif')";
+}
